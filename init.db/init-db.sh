b2e5d2c52c281b47ceb1f50a66685b6bdc9d334c
Vladislav Kartashov <kartashovladislav@gmail.com>
2022-05-21 17:56:16 +0300

    Released vladislavlad/accounts

--- init.db/init-db.sh
+++ init.db/init-db.sh
@@ -9,4 +9,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER munes WITH PASSWORD 'munes';
     CREATE DATABASE munes;
     GRANT ALL PRIVILEGES ON DATABASE munes TO munes;
+
+    CREATE USER trading WITH PASSWORD 'trading';
+    CREATE DATABASE trading;
+    GRANT ALL PRIVILEGES ON DATABASE trading TO trading;
 EOSQL
