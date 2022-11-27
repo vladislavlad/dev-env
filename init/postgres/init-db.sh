4415120373eeeb574a7abbadf42d7cf391ff92e7
Vladislav Kartashov <kartashovladislav@gmail.com>
2022-11-28 00:13:15 +0700

    postgres 15.1

--- init/postgres/init-db.sh
+++ init/postgres/init-db.sh
@@ -3,14 +3,14 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER accounts WITH PASSWORD 'accounts';
-    CREATE DATABASE accounts;
+    CREATE DATABASE accounts WITH OWNER accounts;
     GRANT ALL PRIVILEGES ON DATABASE accounts TO accounts;
 
     CREATE USER munes WITH PASSWORD 'munes';
-    CREATE DATABASE munes;
+    CREATE DATABASE munes WITH OWNER munes;
     GRANT ALL PRIVILEGES ON DATABASE munes TO munes;
 
     CREATE USER trading WITH PASSWORD 'trading';
-    CREATE DATABASE trading;
+    CREATE DATABASE trading WITH OWNER trading;
     GRANT ALL PRIVILEGES ON DATABASE trading TO trading;
 EOSQL
